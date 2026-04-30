f3e9550f6a24c84f0d55b58505ca5ecbdf7f1df3
In-Verse <rosemary@computerpoetry.com>
2026-04-30 16:49:45 -0400

    Hello world prompt mimics a terminal emulator with spacing and cursor fix. More additions.

--- src/assets/js/hello-world.js
+++ src/assets/js/hello-world.js
@@ -1,18 +1,32 @@
-/* All the crazy ways you can say hello */
-/* Many come from the cmdchallenge */
+/* Hello World
+* All the crazy ways you can say hello world
+* Many come from the cmdchallenge
+* You can use hex escape sequences! But it doesn't show well, its like echo $'hello world' so there must be preprocessing
+* echo $'\x68\x65\x6c\x6c\x6f\x20\x77\x6f\x72\x6c\x64'
+* Still contemplating if I should just have the 'echo hello world' */
 const commands = [
 	"echo \"dlrow olleh\" | rev",
 	"cat <(echo \"hello world\")",
 	"cat <<< \"hello world\"",
 	"cat >/dev/stdout <<<\"hello world\"",
-	"i='hello world'; echo $i;",
+	"i='hello world'; echo $i",
 	"awk 'BEGIN { print \"hello world\" }'",
 	"echo \"$(echo hello)\" \"$(echo world)\"",
-	"sed 's.\\.\\...;yhHh\\hh;ywWw\\ww;2,$d' README",
 	"echo ifmmp xpsme |tr bcdefghijklmnopqrstuvwxyza abcdefghijklmnopqrstuvwxyz",
 	"for i in h e l l o; do echo -n $i; done; echo -n \" \"; for j in w o r l d; do echo -n $j; done",
-	"( for i in h e l l o \\  w o r l d ; do echo \"$i\" |awk -F, ' {print $NR}'; done ) |tr -d \\n; echo",
-	"a=(d e h l o r w X \") ; s=(2 1 3 3 4 7 6 4 5 3 0) ; for i in ${s[@]} ; do echo -n ${a[$i]}|tr X\\n ' '; done ; echo \"\""
+	"( for i in h e l l o ' '  w o r l d ; do echo \"$i\" | awk -F, ' {print $NR}'; done ) |tr -d '\\n'",
+	"a=(d e h l o r w X \") ; s=(2 1 3 3 4 7 6 4 5 3 0) ; for i in ${s[@]} ; do echo -n ${a[$i]}|tr X\\n ' '; done ; echo \"\"",
+	"x=hello; y=world; echo ${x} ${y}",
+	"printf '%s' 'hello world'",
+	"tee /dev/null <<< 'hello world'",
+	"echo 'aGVsbG8gd29ybGQ=' | base64 -d",
+	"echo 'NBSWY3DPEB3W64TMMQ======' | base32 -d",
+	"yes 'hello world' | head -n 1",
+	"echo 'h''e''l''l''o' 'w''o''r''l''d'",
+	"date | sed 's/.*/hello world/'",
+	"join <(echo '1 hello') <(echo '1 world') | cut -d ' ' -f2-3",
+	"dd if=/dev/stdin <<< 'Hello World' conv=lcase 2>/dev/null",
+	"head -n 1 <( curl -s www.computerpoetry.com/assets/js/hello-world.js ) | sed 's|/\* ||; s|.*|\L&|'"
 ];
 
 /* Pick a random command */
@@ -42,6 +56,7 @@ function typeText(element, text, speed = 40) {
 }
 
 if (typeof document !== 'undefined') {
+	console.log(commands);
 	/* Looking for the specific elements */
 	const cmdEl = document.querySelector('.terminal .command');
 	const outEl = document.querySelector('.terminal .output');
@@ -50,7 +65,13 @@ if (typeof document !== 'undefined') {
 		typeText(cmdEl, randomCommand, 35).then(() => {
 			if (outEl) {
 				setTimeout(() => {
-					outEl.textContent = '\nhello world';
+					// Every command needs to output 'hello world' 
+					outEl.textContent = '\nhello world\n\n';
+					cmdEl.classList.add('done');
+					const newPrompt = document.createElement('span');
+					// We need to add a new prompt for the next command, otherwise it looks weird (not typical terminal emulator behavior)
+					newPrompt.innerHTML = '<span class="prompt">[thyme@node ~]$</span> <span class="new-prompt"></span>';
+					outEl.after(newPrompt);
 				}, 800);
 			}
 		});
